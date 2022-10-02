63af7a15d04c5c2b70b36a282a68ab0a46f0a8b7
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-10-01 20:53:03 -0400

    test1

--- test_app.py
+++ test_app.py
@@ -8,3 +8,18 @@ from application import app
 def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
+    
+def test_json_data(app.test_client()):
+    response = app.test_client().post("/graphql", json={
+        "query": """
+            query User($id: String!) {
+                user(id: $id) {
+                    name
+                    theme
+                    picture_url
+                }
+            }
+        """,
+        variables={"id": 2},
+    })
+    assert response.json["data"]["user"]["name"] == "Flask"
