81e6c4c62affbf3562faed8a49aca2a43554a57d
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-10-05 13:48:29 -0400

    first part of new test?

--- test_app.py
+++ test_app.py
@@ -9,17 +9,20 @@ def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
     
-def test_json_data(app.test_client()):
-    response = app.test_client().post("/graphql", json={
-        "query": """
-            query User($id: String!) {
-                user(id: $id) {
-                    name
-                    theme
-                    picture_url
-                }
-            }
-        """,
-        variables={"id": 2},
-    })
-    assert response.json["data"]["user"]["name"] == "Flask"
+from flask import Flask, redirect, url_for, render_template 
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+
+@app.route("/<user>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if _name_ == "_main_"
