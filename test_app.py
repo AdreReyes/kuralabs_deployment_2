09b0cc2715950fa307a03f0fae9178b8c831d91d
AdreReyes <108366142+AdreReyes@users.noreply.github.com>
2022-10-08 09:37:39 -0400

    added different test

--- test_app.py
+++ test_app.py
@@ -10,3 +10,8 @@ def test_home_page():
     assert response.status_code == 200
     print("Done.")
     
+@app.route('/users/<user_id>', methods = ['POST'])
+def user(user_id):
+    if request.method == 'POST':
+        """modify/update the information for url-shortener"""
+        data = request.form # a multidict containing POST data
